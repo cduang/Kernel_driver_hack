df0164946cf847a384d7b4f99f91edf9fd6f8cfe
cduang <42569425+cduang@users.noreply.github.com>
2023-10-04 04:07:16 +0800

    Update memory.h

--- kernel/memory.h
+++ kernel/memory.h
@@ -1,5 +1,9 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/version.h>
+#if(LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0))
+#include <linux/sched/mm.h>
+#endif
 
 phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va);
 
