076dada5a16d8c9da0fc8bda811088aa58466bf2
cduang <42569425+cduang@users.noreply.github.com>
2023-10-04 04:08:30 +0800

    Update process.c

--- kernel/process.c
+++ kernel/process.c
@@ -4,14 +4,13 @@
 #include <linux/tty.h>
 #include <linux/mm.h>
 #include <linux/version.h>
-
+#if(LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0))
+#include <linux/sched/mm.h>
+#endif
+    
 #define ARC_PATH_MAX 256
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
-
-#if(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
-extern void mmput(struct mm_struct *);
-#endif
 
 uintptr_t get_module_base(pid_t pid, char* name) 
 {
